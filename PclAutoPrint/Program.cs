ba41d9b9c67c49f334ab3dc0335401af004e2226
Thomas Bellin <tom@catstoday.com>
2022-12-22 17:41:18 -0500

    handle arguments from click-once launcher

--- PclAutoPrint/Program.cs
+++ PclAutoPrint/Program.cs
@@ -12,11 +12,21 @@ namespace PclAutoPrint {
         [STAThread]
         static void Main(string[] args) {
 
+            // if arguments are passed on the command line then we process the files and exit
             if (args.Length > 0) {
                 foreach (string arg in args) FilePrinter.PrintOneFile(arg);
                 return;
             }
 
+            // click-once doesn't pass arguments on the command line
+            if (AppDomain.CurrentDomain.SetupInformation.ActivationArguments.ActivationData.Length > 0) {
+                foreach (string commandLineFile in AppDomain.CurrentDomain.SetupInformation.ActivationArguments.ActivationData) {
+                    MessageBox.Show(string.Format("Command Line File: {0}", commandLineFile));
+                }
+                return;
+            }
+
+            // otherwise display the drag-and-drop form so users can manually send files to the printer
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
